a1f1da73f00b5735c40779aae4cddc23fe3aca83
ragnarlodbrok1992 <maraus92@gmail.com>
2023-06-20 18:37:38 +0200

    WIP

--- test/rdtscpPerformanceCounter.cpp
+++ test/rdtscpPerformanceCounter.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cstdint>
+#include <iostream>
 
 extern "C" uint64_t __rdtscp_proc();
 
@@ -11,6 +12,17 @@ int main() {
 
   uint64_t end = __rdtscp_proc();
   uint64_t elapsed = end - start;
+
+  printf("Elapsed time of measured code: %lld\n", elapsed);
+
+  // ======== Checking how does std::cout performs
+  start = __rdtscp_proc();
+
+  // Measured code goes here
+  std::cout << "How long does it take to print out Mimek engine" << std::endl;
+
+  end = __rdtscp_proc();
+  elapsed = end - start;
 
   printf("Elapsed time of measured code: %lld\n", elapsed);
 
